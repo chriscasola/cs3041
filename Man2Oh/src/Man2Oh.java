6eddf9fac170ab8120149fd0e2d1940bb5676b00
Chris Casola <ccasola@wpi.edu>
2012-11-20 15:47:22 -0500

    Initial commit.

---
+++ Man2Oh/src/Man2Oh.java
@@ -0,0 +1,13 @@
+
+/**
+ * The main class for this help system
+ * @author Chris Casola
+ *
+ */
+public class Man2Oh {
+
+	public static void main(String[] args) {
+		System.out.println("It Ran!");
+	}
+
+}
